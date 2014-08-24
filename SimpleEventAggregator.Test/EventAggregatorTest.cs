dd464b255ad036d34a37ca2992868f95cdb8f138
pcieslik <czolgu@gmail.com>
2014-08-24 15:22:49 +0200

    Test project was added

---
+++ SimpleEventAggregator.Test/EventAggregatorTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SimpleEventAggregator.Test
+{
+    [TestClass]
+    public class EventAggregatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SimpleEventAggregator/EventAggregator.cs
@@ -0,0 +1,7 @@
+﻿
+namespace SimpleEventAggregator
+{
+    public class EventAggregator
+    {
+    }
+}
